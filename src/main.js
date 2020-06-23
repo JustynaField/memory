8eeff3cf76f13249204cfc192230403c92aac042
Justyna <justyna.field@gmail.com>
2020-06-23 12:44:29 -0600

    Layout cards functionality

--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
 var board = document.querySelector('.board');
 
+// 
+// var deck = new Deck;
+//
+// console.log(deck.cards);
 
-var boardHtml = `
-  <div class="row row1">
-    <div class="col-sm-2 card">
-      <img src="./assets/back.jpg" alt="Card Closed" class="card-back">
-    </div>
-    <div class="col-sm-2 card">
-      <img src="./assets/back.jpg" alt="Card Closed" class="card-back">
-    </div>
-    <div class="col-sm-2 card">
-      <img src="./assets/back.jpg" alt="Card Closed" class="card-back">
-    </div>
-    <div class="col-sm-2 card">
-      <img src="./assets/back.jpg" alt="Card Closed" class="card-back">
-    </div>
-  </div>
-`
+function layOutCards () {
+  var rowOne = document.querySelector('.row1');
+  var rowTwo = document.querySelector('.row2');
+  var rowThree = document.querySelector('.row3');
 
-board.innerHTML = boardHtml;
+  var cards = [1, 2, 3, 4, 5, 6];
+
+  cards.forEach((item, i) => {
+
+    var boardHtml = `
+    <div class="col-sm-2 card">
+    <img src="./assets/travel${item}.jpg" alt="Card Opened" class="card-front">
+    <img src="./assets/back.jpg" alt="Card Closed" class="card-back hidden">
+    </div>
+    `
+
+    if (i < 4) {
+      rowOne.innerHTML += boardHtml;
+    }
+    else if (i < 8) {
+      rowTwo.innerHTML += boardHtml;
+    } else {
+      rowThree.innerHTML += boardHtml;
+    }
+  })
+}
+
+layOutCards();
